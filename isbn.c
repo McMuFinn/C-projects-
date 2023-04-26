848db2718848506a6ddc55628f89a3a570578e9e
finn <mcmullenfinn@icloud.com>
2023-04-26 02:12:38 +0200

    updated

--- isbn.c
+++ isbn.c
@@ -8,16 +8,17 @@ int check_isbn10(uint64_t isbn10) {
   if(isbn10 > maxValidISBN||isbn10 < 11){
   return 0;
   }
-//Test
-  uint64_t isbn10Copy = isbn10;
 
+  uint64_t isbn10Copy = isbn10;
   uint64_t mulArr[10] ={0,11, 110, 1100, 11000, 110000, 1100000, 11000000, 110000000, 1100000000};
+  uint64_t powerLookup[8] = {10000000,1000000,100000,10000,1000,100,10,1};  
   uint64_t numDissected[11]; //will hold all digits of the decimal format of the isbn
   uint64_t isbnConverted[10]; //will hold all digits of the actual isbn
   uint64_t currNum;
   uint64_t ergebniss;
   uint64_t crossSum = 0;
-
+  uint64_t currIndex = 7;
+  uint64_t subTotalFor
   for(int i = 0; i < 10; i++){
         numDissected[i] = isbn10Copy % 10; //adds current ones to correct place in array
     	isbn10Copy/=10; //shifts subtotal over one decimal place
@@ -41,6 +42,8 @@ int check_isbn10(uint64_t isbn10) {
    }
         isbnConverted[i] = currNum;
         isbn10Copy -= ergebniss;
+	currNum = isbn10Copy/powerLookup[currIndex];
+	currIndex--;
   }
     
   isbnConverted[0] = isbn10Copy;
